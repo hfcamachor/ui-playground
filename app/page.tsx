ab70606b0cca06c6d1d200ab74a956cc8c9ca373
Hector Camacho <hectorfernandocr@gmail.com>
2023-07-20 20:50:10 -0400

    Hover image page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,95 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [dataMouseDownPoint, setDataMouseDownPoint] = useState<number>(0);
+  const [dataMouseMovePoint, setDataMouseMovePoint] = useState<number>(0);
+  const [percentage, setPercentage] = useState<number>(0);
+  const [lastPercentage, setLastPercentage] = useState<number>(0);
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const imageTracker = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleOnMouseDown);
+    window.addEventListener("mouseup", handleOnMouseUp);
+    window.addEventListener("mousemove", handleOnMouseMove);
+
+    return () => {
+      window.removeEventListener("mousedown", handleOnMouseDown);
+      window.removeEventListener("mouseup", handleOnMouseUp);
+      window.removeEventListener("mousemove", handleOnMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isDragging) {
+      const mouseDelta =
+        parseFloat(String(dataMouseDownPoint)) - dataMouseMovePoint;
+      const maxDelta = window.innerWidth / 2;
+      const percentage =
+        parseFloat(String(lastPercentage)) + (mouseDelta / maxDelta) * -100;
+      if (imageTracker.current && percentage < 0 && percentage > -100) {
+        const imageTrackDiv = imageTracker.current;
+
+        imageTrackDiv.style.transform = `translate(${percentage}%, -50%)`;
+      }
+
+      setPercentage(percentage);
+    }
+  }, [dataMouseDownPoint, dataMouseMovePoint, isDragging]);
+
+  useEffect(() => {
+    if (!isDragging) {
+      setLastPercentage(percentage);
+    }
+    if (percentage > 0) {
+      setLastPercentage(0);
+    } else if (percentage < -100) {
+      setLastPercentage(-100);
+    }
+  }, [isDragging]);
+
+  const handleOnMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleOnMouseDown = (event: any) => {
+    setIsDragging(true);
+    setDataMouseDownPoint(event.clientX);
+    document.documentElement.style.setProperty("--my-variable-name", "pink");
+  };
+
+  const handleOnMouseMove = (event: any) => {
+    setDataMouseMovePoint(event.clientX);
+  };
+
+  const images = () => {
+    const elements = [];
+    for (let index = 1; index <= 8; index++) {
+      elements.push(
+        <div key={`image-${index}`} className={styles.image}>
+          <Image
+            src={`/posters/poster-${index}.jpg`}
+            alt="Vercel Logo"
+            className={styles.image}
+            width={500}
+            height={500}
+            draggable={false}
+          />
+        </div>
+      );
+    }
+    return elements;
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div ref={imageTracker} className={styles.imageTrack}>
+        {images()}
       </div>
     </main>
-  )
+  );
 }
